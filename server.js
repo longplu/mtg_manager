cdaf06b006800dd656621762af7aced5e746337f
Long Lu <longplu@Mac-mini.local>
2021-12-15 13:35:29 -0500

    testing heroku deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const app = express();
 
 // Set up app configuration
-const PORT = process.env.PORT;
+const PORT = app.listen(process.env.PORT || 3000);
 
 // Connect to Database
 mongoose.connect(process.env.DATABASE_URL);
